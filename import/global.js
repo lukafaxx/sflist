c71a4f5262cfbab449b2c4d21958e3b7579b1b50
lukafaxx <lukafaxx@gmail.com>
2015-10-06 16:10:35 +0200

    update footer

--- import/global.js
+++ import/global.js
@@ -2,14 +2,21 @@
 
     // ----- Toggle App Info
     $('#toggle-app').on('click', function(){
-      $('.app-content').slideToggle('slow');
-      var el = $(this);
-      if (el.text() == el.data('text-swap')) {
-        el.text(el.data('text-original'));
-      } else {
-        el.data('text-original', el.text());
-        el.text(el.data('text-swap'));
-      }
+      $('#app-content').slideToggle('slow');
+      $('#donation-content').slideUp('slow');
+      $('#hello-content').slideUp('slow');
+    });
+
+    $('#toggle-donation').on('click', function(){
+      $('#donation-content').slideToggle('slow');
+      $('#app-content').slideUp('slow');
+      $('#hello-content').slideUp('slow');
+    });
+
+    $('#toggle-hello').on('click', function(){
+      $('#hello-content').slideToggle('slow');
+      $('#app-content').slideUp('slow');
+      $('#donation-content').slideUp('slow');
     });
 
   });
